95cf77bd81b2ccfeb06d7d3761250fe30540323f
Arun-hn <94700369+Arun-hn@users.noreply.github.com>
2024-01-02 15:29:18 +0530

    Create main.tf

---
+++ Jenkins_server/main.tf
@@ -0,0 +1,64 @@
+# Define the provider and region
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "jenkins_server" {
+  ami           = "ami-021f7978361c18b01"
+  instance_type = "t2.micro"
+  user_data = file("jenkins-install.sh")
+  user_data_replace_on_change = true
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  key_name = aws_key_pair.jenkins_ssh_key.key_name
+
+  tags = {
+    Name = "Jenkins-Instance"
+  }
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "jenkins_ssh_key" {
+  key_name   = "jenkins_ssh_key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "local_private_key" {
+    content  = tls_private_key.rsa.private_key_pem
+    filename = "local_private_key"
+}
